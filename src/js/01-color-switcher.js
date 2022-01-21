f8a5b7aa7618b65909ade9c553de3cefb5a09576
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-21 17:46:48 +0200

    first task started

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtn.style.disabled = true;
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  startBtn.style.disabled = false;
+  clearInterval(timerId);
+  isStarted = false;
+}
